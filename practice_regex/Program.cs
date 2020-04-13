dcbfe59610203926e6fe3467becaafcc2c19925b
mai1121 <maimaiukawa@gmail.com>
2020-04-13 19:46:24 +0900

    first commit

---
+++ practice_regex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
